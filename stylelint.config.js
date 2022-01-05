382965e633da24a8d02a515f555add42a25ce1ae
Victor Getsov <victorgetsov@gmail.com>
2022-01-05 23:32:15 +0200

    add prettier, stylelint and eslint

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-standard-scss',
+        'stylelint-config-sass-guidelines',
+        'stylelint-prettier/recommended',
+    ],
+    rules: {
+        'order/properties-alphabetical-order': null,
+        'max-nesting-depth': null,
+        'color-hex-length': 'long',
+        'selector-no-qualifying-type': null,
+    },
+}
