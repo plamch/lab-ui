619ab7598ddb819f8c08908a4691ef67bbe10082
Victor Getsov <viktorgetsov@gmail.com>
2022-01-07 04:45:46 +0200

    try adding react-router

---
+++ src/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function About() {
+    return (
+        <div>
+            <p>There's nothing to see here yet</p>
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
 import './App.scss'
+import { Counter } from './features/counter/Counter'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { About } from './About'
 
 function App() {
     return (
         <div className="App">
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                    <Routes>
+                        <Route path="/" component={<App />} />
+                        <Route path="/about" component={<About />} />
+                    </Routes>
+                </div>
+            </Router>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <Counter />
