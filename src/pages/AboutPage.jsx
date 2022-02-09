da1ff7d115088b937fb9569cf9f6dc232fe71ca7
Victor Getsov <viktorgetsov@gmail.com>
2022-02-10 00:25:22 +0200

    integrate ui with services

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,35 @@
 import { Helmet } from 'react-helmet-async'
+import { useState, useEffect } from 'react'
 
 export function About() {
+    const [greeting, setGreeting] = useState('hi')
+    // useEffect(() => {
+    //     const getGreeting = async () => {
+    //         const helloResponse = await fetch('http://localhost:4000/test/greeting?greet=bonjour')
+    //         const helloText = await helloResponse.json()
+    //         setGreeting(helloText.greeting)
+    //     }
+    //     getGreeting()
+    // }, [])
+
+    const onGetGreetingClick = async () => {
+        const helloResponse = await fetch('http://localhost:4000/hello')
+        const helloText = await helloResponse.json()
+        setGreeting(helloText.allUsers)
+    }
+
     return (
         <>
             <Helmet>
                 <title>About Lab</title>
             </Helmet>
             <div>
-                <p>There's nothing to see here yet</p>
+                <button onClick={onGetGreetingClick}>Click me</button>
+                <div>
+                    {greeting?.map?.(({ email }) => (
+                        <div key={email}>{email}</div>
+                    ))}
+                </div>
             </div>
         </>
     )
